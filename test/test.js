9080463a5de8ce247f8faab3d495cdfae8fad360
Richard Blaauw <richard.blaauw@ogd.nl>
2020-10-17 13:31:18 +0200

    add a test to see if app.js is present in the build folder and being served by the front end server; delete no longer necessary files.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-describe ('API', function() {
+describe ('API is serving data', function() {
     it('status', function(done){
         request('http://localhost:8080/api/getbalance', function(error, response, body) {
             expect(response.statusCode).to.equal(200);
@@ -10,18 +10,27 @@ describe ('API', function() {
     });
 });
 
-describe ('Account Info', function() {
+describe ('index.html is present in build folder', function() {
     it('status', function(done){
-        request('http://localhost:8080/api/getbalance', function(error, response, body) {
+        request('http://localhost:3000/index.html', function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             done();
         });
     });
 });
 
-describe ('Transactions', function() {
+describe ('css is present in build folder', function() {
     it('status', function(done){
-        request('http://localhost:8080/api/getbalance', function(error, response, body) {
+        request('http://localhost:3000/css/main.css', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
+
+describe ('js is present in build folder', function() {
+    it('status', function(done){
+        request('http://localhost:3000/js/app.js', function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             done();
         });
